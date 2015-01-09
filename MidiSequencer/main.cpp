22a8b91fcebcf7963cede74a5f2d503eac5037c3
Guillermo <guillermoechegoyenblanco@gmail.com>
2015-01-09 14:42:09 +0100

    Initial Commit

---
+++ MidiSequencer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MidiSequencer
+//
+//  Created by Guillermo on 09/01/15.
+//  Copyright (c) 2015 Guillermo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
